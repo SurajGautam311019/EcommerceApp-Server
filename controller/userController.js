51ed83f2f4328d2ac31a39c730bd1c46218fd6b9
SurajGautam311019 <surajgautam3110@gmail.com>
2023-12-10 22:32:38 +0530

    added create user api

---
+++ controller/userController.js
@@ -0,0 +1,20 @@
+const User=require('../models/userModel');
+
+const createUser=async (req,res)=>{
+    const email=req.body.email;
+    const findUser=await User.findOne({email:email});
+    if(!findUser){
+        //create new user
+        const newUser=await User.create(req.body);
+        res.json(newUser);
+    }
+    else{
+        //user already exist
+        res.json({
+            message:"user already exist",
+            success:false
+        })
+    }
+}
+
+module.exports={createUser};
--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const dbConnect = require("./config/dbConnect");
 const app=express();
 const dotenv=require("dotenv").config();
 const PORT=process.env.PORT || 8000;
+const authRouter=require("./routes/authRoute");
+const bodyParser = require("body-parser");
 
 dbConnect();
-app.use('/',(req,res)=>{
-    res.send('hello from server')
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}))
+app.use('/user',authRouter);
+
+
 app.listen(PORT,()=>{
     console.log(`app is listening at PORT ${PORT}`);
 })
